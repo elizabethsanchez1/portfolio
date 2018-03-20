68284a52314a62bc9a0a4e654140aca15d60f071
elizabethsanchez1 <elizabethsanchez1@live.com>
2018-03-20 18:27:49 -0500

    updated navigation

---
+++ main.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+    
+    var nav = document.getElementById('nav');
+
+        window.scroll = function(){
+            
+            if (window.pageYOffset >100) {
+                
+                nav.style.background = "#BDBD9C";
+            }
+            else {
+                nav.style.background = "transparent";
+            }
+        }
